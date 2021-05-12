0662133a6ee95b5171819d7f06d304f720d3de52
Byron Hall <byron.hall@ricardodefense.com>
2021-05-12 12:46:56 -0700

    created job. TODO: place job into queue

---
+++ triggers/WebApplication1/WebApplication1/Jobs/CheckAvailability.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Quartz;
+using System.Diagnostics;
+
+namespace WebApplication1.Jobs
+{
+    public class CheckAvailability : IJob
+    {
+        Task IJob.Execute(IJobExecutionContext context)
+        {
+            try
+            {
+                Debug.WriteLine("Opponent is not available.");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
