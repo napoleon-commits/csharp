9a21165041693caf3de1b175b47ee48950f5483c
Byron Hall <byron.hall@ricardodefense.com>
2021-06-04 15:42:06 -0700

    Database First Demo

---
+++ DatabaseFirstDemo/DatabaseFirstDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new ldbtestcctEntities();
+            var xmlConfig = new xml_config();
+            context.xml_config.Add(xmlConfig);
+            context.SaveChanges();
+        }
+    }
+}
