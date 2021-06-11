0ea01b6b614f7d64042d3ce018986cc272aa8d95
Byron Hall <byron.hall@ricardodefense.com>
2021-06-11 07:04:17 -0700

    xpath

---
+++ xpath/xpath/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Xml;
+
+namespace xpath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                string path = "C:\\xmls\\foo.xml";
+                doc.Load(path);
+                var result = doc.SelectNodes("/note/to");
+                foreach (XmlNode node in result)
+                {
+                    Console.WriteLine(node.InnerText);
+                }
+                result = doc.SelectNodes("/note[to='Tove']/to");
+                foreach (XmlNode node in result)
+                {
+                    Console.WriteLine(node.InnerText);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("{0}", e.ToString());
+            };
+        }
+    }
+}
